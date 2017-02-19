36d7e1cb58cd8ceb53801a1217b7400ee8794700
Jamie Potter <jamie.potter.16@ucl.ac.uk>
2017-02-19 14:30:42 +0000

    replace magic numbers

--- boids/bad_boids.py
+++ boids/bad_boids.py
@@ -6,11 +6,13 @@ import numpy as np
 
 # Deliberately terrible code for teaching purposes
 
+boid_count = range(50)
+
 def new_flock():
-    boids_x = [random.uniform(-450, 50.0) for x in range(50)]
-    boids_y = [random.uniform(300.0, 600.0) for x in range(50)]
-    boid_x_velocities = [random.uniform(0, 10.0) for x in range(50)]
-    boid_y_velocities = [random.uniform(-20.0, 20.0) for x in range(50)]
+    boids_x = [random.uniform(-450, 50.0) for x in boid_count]
+    boids_y = [random.uniform(300.0, 600.0) for x in boid_count]
+    boid_x_velocities = [random.uniform(0, 10.0) for x in boid_count]
+    boid_y_velocities = [random.uniform(-20.0, 20.0) for x in boid_count]
     boids = (boids_x, boids_y, boid_x_velocities, boid_y_velocities)
     return boids
 
@@ -21,32 +23,32 @@ def update_boids(boids):
     
     # Fly towards the middle
     
-    for i in range(50):
-        for j in range(50):
+    for i in boid_count:
+        for j in boid_count:
             xvs[i] = xvs[i]+(xs[j]-xs[i])*0.01/len(xs)
-    for i in range(50):
-        for j in range(50):
+    for i in boid_count:
+        for j in boid_count:
             yvs[i] = yvs[i]+(ys[j]-ys[i])*0.01/len(xs)
     
     # Fly away from nearby boids
     
-    for i in range(50):
-        for j in range(50):
+    for i in boid_count:
+        for j in boid_count:
             if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
                 xvs[i] = xvs[i]+(xs[i]-xs[j])
                 yvs[i] = yvs[i]+(ys[i]-ys[j])
                 
     # Try to match speed with nearby boids
     
-    for i in range(50):
-        for j in range(50):
+    for i in boid_count:
+        for j in boid_count:
             if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 <10000:
                 xvs[i] = xvs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
                 yvs[i] = yvs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
                 
     # Move according to velocities
     
-    for i in range(50):
+    for i in boid_count:
         xs[i] = xs[i]+xvs[i]
         ys[i] = ys[i]+yvs[i]
 
