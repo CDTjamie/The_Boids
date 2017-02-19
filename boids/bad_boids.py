d2580c8fe9e909539581379f379ed8a4c9f97834
Jamie Potter <jamie.potter.16@ucl.ac.uk>
2017-02-19 17:32:26 +0000

    proximity function

--- boids/bad_boids.py
+++ boids/bad_boids.py
@@ -18,6 +18,9 @@ def new_flock():
 
 boids = new_flock()
 
+def proximity(i, j, xs, ys, xvs, yvs, distance):
+    return (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < distance
+
 def fly_towards_middle(i, j, xs, ys, xvs, yvs):
     xvs[i] = xvs[i]+(xs[j]-xs[i])*(0.01/len(xs))
     yvs[i] = yvs[i]+(ys[j]-ys[i])*(0.01/len(xs))
@@ -25,14 +28,14 @@ def fly_towards_middle(i, j, xs, ys, xvs, yvs):
     return xvs[i], yvs[j]
 
 def avoid_boids(i, j, xs, ys, xvs, yvs):
-    if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
+    if proximity(i,j,xs,ys,xvs,yvs,100):
         xvs[i] = xvs[i]+(xs[i]-xs[j])
         yvs[i] = yvs[i]+(ys[i]-ys[j])
         
     return xvs[i], yvs[i]
 
 def match_speed(i, j, xs, ys, xvs, yvs):
-    if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 <10000:
+    if proximity(i,j,xs,ys,xvs,yvs,10000):
         xvs[i] = xvs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
         yvs[i] = yvs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
         
