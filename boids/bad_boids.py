f65dc862d9b0e88c8999aa56c358dcbd8be52574
Jamie Potter <jamie.potter.16@ucl.ac.uk>
2017-02-22 11:55:19 +0000

    another class

--- boids/bad_boids.py
+++ boids/bad_boids.py
@@ -7,7 +7,7 @@ import yaml
 
 # Deliberately terrible code for teaching purposes
 
-config = yaml.load(open("boids/config.yaml"))
+config = yaml.load(open("config2.yaml"))
 boid_number = config["boid_number"]
 x_position_limits = config["x_position_limits"]
 y_position_limits = config["y_position_limits"]
@@ -43,55 +43,68 @@ class Flock(object):
 myflock = Flock(boid_number, x_position_limits, y_position_limits, x_velocity_limits, y_velocity_limits)
 boid_positions, boid_velocities = myflock.new_flock()
 
-def proximity(i, j, boid_positions, boid_velocities, distance):
-    return (boid_positions[0][j]-boid_positions[0][i])**2 + (boid_positions[1][j]-boid_positions[1][i])**2 < distance
-
-def fly_towards_middle(i, j, boid_positions, boid_velocities):
-    boid_velocities[0][i] = boid_velocities[0][i]+(boid_positions[0][j]-boid_positions[0][i])*middle_scaling/boid_number
-    boid_velocities[1][i] = boid_velocities[1][i]+(boid_positions[1][j]-boid_positions[1][i])*middle_scaling/boid_number
-    
-    return boid_positions, boid_velocities
-
-def avoid_boids(i, j, boid_positions, boid_velocities):
-    if proximity(i,j,boid_positions,boid_velocities,avoid_distance):
-        boid_velocities[0][i] = boid_velocities[0][i]+(boid_positions[0][i]-boid_positions[0][j])
-        boid_velocities[1][i] = boid_velocities[1][i]+(boid_positions[1][i]-boid_positions[1][j])
-        
-    return boid_positions, boid_velocities
-
-def match_speed(i, j, boid_positions, boid_velocities):
-    if proximity(i,j,boid_positions,boid_velocities,match_speed_distance):
-        boid_velocities[0][i] = boid_velocities[0][i]+(boid_velocities[0][j]-boid_velocities[0][i])*match_scaling/boid_number
-        boid_velocities[1][i] = boid_velocities[1][i]+(boid_velocities[1][j]-boid_velocities[1][i])*match_scaling/boid_number
-        
-    return boid_positions, boid_velocities
-
-def move(boid_positions, boid_velocities, i):
-    boid_positions[0][i] = boid_positions[0][i]+boid_velocities[0][i]
-    boid_positions[1][i] = boid_positions[1][i]+boid_velocities[1][i]
-    
-    return boid_positions
-
-def update_boids(boid_positions, boid_velocities):
-    boids = range(boid_number)
-    
-    for i in boids:
-        for j in boids:
-            fly_towards_middle(i,j,boid_positions, boid_velocities)
-            avoid_boids(i,j,boid_positions, boid_velocities)
-            match_speed(i,j,boid_positions, boid_velocities)
-                
-    for i in boids:
-        boid_positions = move(boid_positions, boid_velocities, i)
-        
-    return boid_positions, boid_velocities
-
 figure = plt.figure()
 axes = plt.axes(xlim=(-500, 1500), ylim=(-500,1500))
 scatter = axes.scatter(boid_positions[0], boid_positions[1])
 
+
+class Flight(object):
+    def __init__(self, boid_number, boid_positions, boid_velocities, avoid_distance, match_speed_distance, middle_scaling, match_scaling):
+        self.boid_number = boid_number
+        self.boid_positions = boid_positions
+        self.boid_velocities = boid_velocities
+        self.avoid_distance = avoid_distance
+        self.match_speed_distance = match_speed_distance
+        self.middle_scaling = middle_scaling
+        self.match_scaling = match_scaling
+        
+    def proximity(self, i, j, boid_positions, boid_velocities, distance):
+        return (boid_positions[0][j]-boid_positions[0][i])**2 + (boid_positions[1][j]-boid_positions[1][i])**2 < distance
+    
+    def fly_towards_middle(self, i, j, boid_positions, boid_velocities):
+        boid_velocities[0][i] = boid_velocities[0][i]+(boid_positions[0][j]-boid_positions[0][i])*self.middle_scaling/self.boid_number
+        boid_velocities[1][i] = boid_velocities[1][i]+(boid_positions[1][j]-boid_positions[1][i])*self.middle_scaling/self.boid_number
+    
+        return boid_positions, boid_velocities
+        
+    def avoid_boids(self, i, j, boid_positions, boid_velocities):
+        if self.proximity(i,j,boid_positions,boid_velocities,self.avoid_distance):
+            boid_velocities[0][i] = boid_velocities[0][i]+(boid_positions[0][i]-boid_positions[0][j])
+            boid_velocities[1][i] = boid_velocities[1][i]+(boid_positions[1][i]-boid_positions[1][j])
+        
+        return boid_positions, boid_velocities
+    
+    def match_speed(self, i, j, boid_positions, boid_velocities):
+        if self.proximity(i,j,boid_positions,boid_velocities,self.match_speed_distance):
+            boid_velocities[0][i] = boid_velocities[0][i]+(boid_velocities[0][j]-boid_velocities[0][i])*self.match_scaling/self.boid_number
+            boid_velocities[1][i] = boid_velocities[1][i]+(boid_velocities[1][j]-boid_velocities[1][i])*self.match_scaling/self.boid_number
+        
+        return boid_positions, boid_velocities
+    
+    def move(self, boid_positions, boid_velocities, i):
+        boid_positions[0][i] = boid_positions[0][i]+boid_velocities[0][i]
+        boid_positions[1][i] = boid_positions[1][i]+boid_velocities[1][i]
+    
+        return boid_positions
+    
+    def update_boids(self):
+        boids = range(self.boid_number)
+    
+        for i in boids:
+            for j in boids:
+                self.fly_towards_middle(i,j,self.boid_positions, self.boid_velocities)
+                self.avoid_boids(i,j,self.boid_positions, self.boid_velocities)
+                self.match_speed(i,j,self.boid_positions, self.boid_velocities)
+                
+        for i in boids:
+            boid_positions = self.move(self.boid_positions, self.boid_velocities, i)
+        
+        return boid_positions, boid_velocities
+
+myflight = Flight(boid_number, boid_positions, boid_velocities, avoid_distance, match_speed_distance, middle_scaling, match_scaling)
+
 def animate(frame):
-    update_boids(boid_positions, boid_velocities)
+    boid_positions, boid_velocities = myflight.update_boids()
     x_pos = np.array(boid_positions[0])
     y_pos = np.array(boid_positions[1])
     data = np.hstack((x_pos[:,np.newaxis], y_pos[:, np.newaxis]))
