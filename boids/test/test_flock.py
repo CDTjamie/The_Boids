5782bd5809c48f36a3333584455b9cf9045ca0ba
Jamie Potter <jamie.potter.16@ucl.ac.uk>
2017-02-23 19:40:48 +0000

    Flock test file

--- boids/test/test_flock.py
+++ boids/test/test_flock.py
@@ -20,3 +20,24 @@ def test_init():
     match_scaling = config["match_scaling"]
     
     with assert_raises(TypeError) as exception: Flock(boid_number, x_position_limits, y_position_limits, x_velocity_limits, y_velocity_limits)
+        
+def test_random():
+    _ROOT = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(_ROOT,'config_correct.yaml')) as config_file:
+        config = yaml.load(config_file)
+        
+    boid_number = config["boid_number"]
+    x_position_limits = config["x_position_limits"]
+    y_position_limits = config["y_position_limits"]
+    x_velocity_limits = config["x_velocity_limits"]
+    y_velocity_limits = config["y_velocity_limits"]
+    avoid_distance = config["avoid_distance"]
+    match_speed_distance = config["match_speed_distance"]
+    middle_scaling = config["middle_scaling"]
+    match_scaling = config["match_scaling"]
+    
+    temp_flock = Flock(boid_number, x_position_limits, y_position_limits, x_velocity_limits, y_velocity_limits)
+    values = temp_flock.initialise(x_position_limits, range(boid_number))
+    
+    assert type(values) == list
+    assert type(values[0]) == float
