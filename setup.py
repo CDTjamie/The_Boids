366c400108ba5061cc25ed87e3c31fc650213dbe
Jamie Potter <jamie.potter.16@ucl.ac.uk>
2017-02-23 12:00:33 +0000

    setup file - config fix

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 
 from setuptools import setup, find_packages
 
-setup(name = "The_Boids", version = "0.1.0", packages = find_packages(exclude=['*test']), scripts = ['scripts/boids'], install_requires = ['numpy', 'matplotlib'])
+setup(name = "The_Boids", version = "0.1.0", packages = find_packages(exclude=['*test']), scripts = ['scripts/boids'], install_requires = ['numpy', 'matplotlib'], include_package_data = True)
