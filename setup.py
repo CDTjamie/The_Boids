b0adf94d5e3aa58a463685bc851e5457959dba1b
Jamie Potter <jamie.potter.16@ucl.ac.uk>
2017-02-23 13:06:06 +0000

    setup file - metadata

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(name = "The_Boids",
       version = "0.1.0", 
       description = "refactored Python flocking code",
-      author = "Jamie Potter"
+      author = "Jamie Potter",
       packages = find_packages(exclude=['*test']), 
       scripts = ['scripts/boids'], 
       install_requires = ['numpy', 'matplotlib', 'pyyaml', 'pyrandom'], 
