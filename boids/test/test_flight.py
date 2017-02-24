c42b4263a5c5cf6a79174699ab9f56b62fbaa9c8
Jamie Potter <jamie.potter.16@ucl.ac.uk>
2017-02-24 11:53:11 +0000

    Flight test file

---
+++ boids/test/test_flight.py
@@ -0,0 +1,26 @@
+
+from nose.tools import assert_raises, assert_equal
+from boids.flight import Flight
+import yaml
+import os
+
+def test_init():
+    _ROOT = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(_ROOT,'config_correct.yaml')) as config_file:
+        config = yaml.load(config_file)
+        
+    boid_number = config["boid_number"]
+    x_position_limits = config["x_position_limits"]
+    y_position_limits = config["y_position_limits"]
+    x_velocity_limits = config["x_velocity_limits"]
+    y_velocity_limits = config["y_velocity_limits"]
+    avoid_distance = config["avoid_distance"]
+    match_speed_distance = config["match_speed_distance"]
+    middle_scaling = config["middle_scaling"]
+    match_scaling = config["match_scaling"]
+    
+    boid_positions = 50.0
+    boid_velocities = "velocity"
+    
+    with assert_raises(TypeError) as exception: Flight(boid_number, ([5.,5.],[5.,5.]), boid_velocities, avoid_distance, match_speed_distance, middle_scaling, match_scaling)
+    with assert_raises(TypeError) as exception: Flight(boid_number, boid_positions, ([5.,5.],[5.,5.]), avoid_distance, match_speed_distance, middle_scaling, match_scaling)
